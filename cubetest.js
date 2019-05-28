df0c8cf57fa402f1bc5ac049d83d06baaabc1b94
William Herald Snyder <will.herald.snyder@gmail.com>
2019-05-28 18:40:42 -0400

    rotating pyr with uv cross origin bs overwith...

--- cubetest.js
+++ cubetest.js
@@ -4,13 +4,22 @@ precision mediump float;
 
 attribute vec3 vertpos;
 attribute vec3 vertcolor;
+attribute vec2 texCoord;
 
 uniform mat4 viewmat;
 uniform float timevar;
 uniform mat4 frust;
 
 
-varying vec3 fragcolor;
+/*struct Data {
+  vec2 tex;
+  vec3 color;
+};
+
+varying Data out;*/
+
+varying vec2 tex;
+varying vec3 color;
 
 
 void main(){
@@ -18,25 +27,35 @@ void main(){
     //gl_Position = vec4(vertpos.x, sin(timevar+vertpos.x)*vertpos.y, 1.0, 1.0 + abs(cos(timevar)));
     //fragcolor = vec3(abs(sin(timevar + vertcolor.x)), abs(sin(timevar + .5 + vertpos.y)), abs(sin(timevar + 1.0)));
 
-    fragcolor = vertcolor;
+    //fragcolor = vertcolor;
     //gl_Position = vec4(vertpos, 1.0);
 
     //fragcolor = vec3(abs(sin(timevar + vertcolor.x)), abs(sin(timevar + .5 + vertpos.y)), abs(sin(timevar + 1.0)));
     gl_Position = frust * viewmat * vec4(vertpos, 1.0);
+
+    tex = texCoord;
+    color = vertcolor;
 }`;
+
 
 
 var frag_shader = `
 
 precision mediump float;
+uniform sampler2D sampler;
 
-varying vec3 fragcolor;
+//varying Data out;
 
+varying vec2 tex;
+varying vec3 color;
 
 void main(){
-
-    gl_FragColor = vec4(fragcolor, 1.0);
+    gl_FragColor = vec4(color, 1.0) * texture2D(sampler, tex);
 }`;
+
+
+
+
 
 /*
 class Vert {
@@ -147,9 +166,9 @@ function initShader(gl, shaderVar, shaderText){
     return 0;
 }
 
-var eyeCoord = vec3.fromValues(0,2,2);
+var eyeCoord = vec3.fromValues(0,3,0);
 var focusPt = vec3.fromValues(0,0,-3);
-var upDir = vec3.fromValues(1,1,0);
+var upDir = vec3.fromValues(0,1,0);
 var recv = vec3.create();
 var rotationQuat = quat.create(); 
 quat.fromEuler(rotationQuat, 0,0,0);
@@ -166,6 +185,7 @@ mat4.perspective(frust, Math.PI/2, 4/3, .5, 10);
 var viewmatrix =  mat4.create();
 var viewmat, frustmat, rotmat;
 var timemem, time;
+var samplermem;
 
 var cont = 1;
 var dir = 0;
@@ -208,25 +228,70 @@ function render(){
 
 document.onkeydown = function(ev){ trigger(ev); };
     
-    function trigger(ev) {
+function trigger(ev) {
 
-        if (ev.keyCode == 39){
-            dir += 1;
-        }
-        else if (ev.keyCode == 37){
-            dir -= 1;
-        }
-        else if (ev.keyCode == 81){//q
-            cont = 0;
-        }
-        else if (ev.keyCode == 87){//w
+    if (ev.keyCode == 39){
+        dir += 1;
+    }
+    else if (ev.keyCode == 37){
+        dir -= 1;
+    }
+    else if (ev.keyCode == 81){//q
+        cont = 0;
+    }
+    else if (ev.keyCode == 87){//w
 
-            cont = 1;
-            currTime = performance.now();
-            window.requestAnimationFrame(render);
-        }
-        else {return;}
-    };
+        cont = 1;
+        currTime = performance.now();
+        window.requestAnimationFrame(render);
+    }
+    else {return;}
+};
+
+
+
+
+function loadTexture(gl, img){
+
+    console.log("up here width is: " + img.width);
+
+    var texture = gl.createTexture();
+
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+
+    //var image = createTextureFromImage(gl, img);
+    //^^texImg2D called in that func
+
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    gl.uniform1i(samplermem, 0);
+}
+
+
+
+function initTexture(gl){
+
+    var img = new Image();
+
+    img.onload = function(){ loadTexture(gl, img);};
+
+    img.crossOrigin = "anonymous"; 
+    img.src = "./images/foam.jpg";
+}
+
+
+
+
 
 
 var cubedemo = function () {
@@ -256,11 +321,11 @@ var cubedemo = function () {
 
     var verts = new Float32Array([
     // Vertex coordinates and color
-        -1.0,  0.0,  -5.0,     1.0,  1.0,  1.0,  // v0 White
-        1.0,  0.0,  -5.0,     1.0,  0.0,  1.0,  // v1 Magenta
-        0.0,   3.0,  -3.0,    1.0,  0.0,  0.0,  // v2 Red
-        -1.0, 0.0,  -1.0,     1.0,  1.0,  0.0,  // v3 Yellow
-        1.0,  0.0, -1.0,     0.0,  1.0,  0.0,  // v4 Green
+        -1.0,  0.0,  -5.0,     1.0,  1.0,  1.0,  0.0,0.0,// v0 White
+        1.0,  0.0,  -5.0,     1.0,  0.0,  1.0,  1.0,0.0,// v1 Magenta
+        0.0,   3.0,  -3.0,    1.0,  0.0,  0.0,  0.0,0.0,// v2 Red
+        -1.0, 0.0,  -1.0,     1.0,  1.0,  0.0,  0.0,1.0,// v3 Yellow
+        1.0,  0.0, -1.0,     0.0,  1.0,  0.0,  1.0,1.0// v4 Green
     ]);
 
     // Indices of the vertices
@@ -280,12 +345,25 @@ var cubedemo = function () {
 
     var posMem = gl.getAttribLocation(prog, 'vertpos');
     var colorMem = gl.getAttribLocation(prog, 'vertcolor');
+    var texMem = gl.getAttribLocation(prog, 'texCoord');
 
-    gl.vertexAttribPointer(posMem, 3, gl.FLOAT, gl.FALSE, elSize * 6, 0);
-    gl.vertexAttribPointer(colorMem, 3, gl.FLOAT, gl.FALSE, elSize * 6, 3 * elSize);
+    samplermem = gl.getUniformLocation(prog, 'sampler');
+
+
+    gl.vertexAttribPointer(posMem, 3, gl.FLOAT, gl.FALSE, elSize * 8, 0);
+    gl.vertexAttribPointer(colorMem, 3, gl.FLOAT, gl.FALSE, elSize * 8, 3 * elSize);
+    gl.vertexAttribPointer(texMem, 2, gl.FLOAT, gl.FALSE, elSize * 8, 6 * elSize);
 
     gl.enableVertexAttribArray(posMem);
     gl.enableVertexAttribArray(colorMem);
+
+
+    initTexture(gl);
+
+
+    gl.enableVertexAttribArray(texMem);
+
+
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indBuf);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
@@ -294,6 +372,7 @@ var cubedemo = function () {
 
     viewmat = gl.getUniformLocation(prog, 'viewmat');
     frustmat = gl.getUniformLocation(prog, 'frust');
+    
 
     gl.uniformMatrix4fv(frustmat, false, frust);
 
