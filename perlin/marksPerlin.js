49c6921260f582dc3046b37221d6dd0b461cd500
William Herald Snyder <will.herald.snyder@gmail.com>
2019-10-31 15:37:08 -0400

    much better noise function, though not enough clumping

--- perlin/marksPerlin.js
+++ perlin/marksPerlin.js
@@ -24,18 +24,21 @@ BEGIN MARK'S CODE
 
 */
 
+var dataUpdateFrag = 
+`#version 300 es
 
+precision mediump float;
+precision mediump sampler2D;
 
-var fboFrag = `
-
-// simulation
-varying vec2 vUv;
-
-uniform sampler2D tPositions;
-uniform sampler2D origin;
-
+uniform sampler2D startTex;
 uniform float timer;
-uniform vec3 velocity;
+
+
+in vec2 index;
+
+layout(location=0) out vec4 newPosition;
+layout(location=1) out vec4 newVelocity;
+
 
 vec4 mod289(vec4 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0; 
@@ -93,10 +96,10 @@ float snoise(vec4 v) {
   vec4 i0;
   vec3 isX = step( x0.yzw, x0.xxx );
   vec3 isYZ = step( x0.zww, x0.yyz );
-  //  i0.x = dot( isX, vec3( 1.0 ) );
+
   i0.x = isX.x + isX.y + isX.z;
   i0.yzw = 1.0 - isX;
-  //  i0.y += dot( isYZ.xy, vec2( 1.0 ) );
+
   i0.y += isYZ.x + isYZ.y;
   i0.zw += 1.0 - isYZ.xy;
   i0.z += isYZ.z;
@@ -107,11 +110,7 @@ float snoise(vec4 v) {
   vec4 i2 = clamp( i0-1.0, 0.0, 1.0 );
   vec4 i1 = clamp( i0-2.0, 0.0, 1.0 );
 
-  //  x0 = x0 - 0.0 + 0.0 * C.xxxx
-  //  x1 = x0 - i1  + 1.0 * C.xxxx
-  //  x2 = x0 - i2  + 2.0 * C.xxxx
-  //  x3 = x0 - i3  + 3.0 * C.xxxx
-  //  x4 = x0 - 1.0 + 4.0 * C.xxxx
+  
   vec4 x1 = x0 - i1 + C.xxxx;
   vec4 x2 = x0 - i2 + C.yyyy;
   vec4 x3 = x0 - i3 + C.zzzz;
@@ -128,6 +127,7 @@ float snoise(vec4 v) {
 
   // Gradients: 7x7x6 points over a cube, mapped onto a 4-cross polytope
   // 7*7*6 = 294, which is close to the ring size 17*17 = 289.
+
   vec4 ip = vec4(1.0/294.0, 1.0/49.0, 1.0/7.0, 0.0) ;
 
   vec4 p0 = grad4(j0,   ip);
@@ -153,65 +153,25 @@ float snoise(vec4 v) {
          + dot(m1*m1, vec2( dot( p3, x3 ), dot( p4, x4 ) ) ) ) ;
 }
 
-
-void main() {
-  vec3 pos = texture2D( tPositions, vUv ).xyz;
-  
-  vec3 inc = vec3(0.0);
-  inc.x = snoise(vec4(pos.x, pos.y, pos.z, timer/10000.0)) * 0.01;
-  inc.y = snoise(vec4(pos.x, pos.y, pos.z, 1.352+timer/10000.0)) * 0.01;
-  inc.z = snoise(vec4(pos.x, pos.y, pos.z, 12.352+timer/10000.0)) * 0.01;
-
-  pos += inc;
-
-  // Write new position out
-  gl_FragColor = vec4(pos, 1.0);
-}`
-
-
-
-var fboRenderVert = `
-uniform sampler2D map;
-
-uniform float width;
-uniform float height;
-
-uniform float pointSize;
-
-varying vec2 vUv;
-varying vec4 vPosition;
-
-
-/* Pseudo random number generator
 float rand(vec2 co){
   return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 }
-*/
 
-
-void main(){
-  vUv = position.xy + vec2( 0.5 / width, 0.5 / height );
-
-  vec3 position = ( texture2D( map, vUv ).rgb  );
-
-  gl_PointSize = 2.0;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-}`
-
-
-var fboRenderFrag = `
-
-uniform sampler2D map;
-uniform float effector;
-
-varying vec2 vUv;
-varying vec4 vPosition;
 
 void main() {
-  gl_FragColor = vec4( 0.2,0.1,0.8,1.0 );
-  gl_FragColor *= 1.5;
-}`
+  vec3 randoffset = vec3(rand(vec2(timer,index.x)), rand(vec2(index.y,timer/10.0)), rand(vec2(timer,index.x)))/1000.0;
+  vec3 pos = (texture( startTex, index ).xyz + randoffset); 
+  
+  vec3 inc = vec3(0.0);
+  inc.x = snoise(vec4(pos.x, pos.y, pos.z, timer/10000.0)) * 0.01;
+  inc.y = snoise(vec4(pos.y, pos.x, pos.z, 1.352+timer/10000.0)) * 0.01;
+  inc.z = snoise(vec4(pos.z, pos.y, pos.x, 12.352+timer/10000.0)) * 0.01;
 
+  pos = 1.5 * pos + 50.0 * inc;
+
+  newPosition = vec4(pos, 1.0);
+  newVelocity = vec4(50.0 * inc, 1.0);
+}`
 
 /*
 
@@ -250,7 +210,7 @@ out vec4 position;
 void main(){
 
   	vec4 pos = texture(dataTex, dataIndex/numParticles);
-  	color = abs(normalize(vec4(texture(velTex, dataIndex/numParticles).xyz,.75)));
+    color = vec4(abs(normalize(texture(velTex, dataIndex/numParticles).xyz)),1.0);
 
   	vec4 plusRadius = pos + vec4(radius, 0.0, 0.0, 0.0);
 
@@ -309,7 +269,6 @@ uniform sampler2D dataTex;
 uniform sampler2D velTex;
 
 uniform mat4 mvp;
-//uniform mat4 frust;
 uniform float numParticles;
 
 layout(location=0) in vec2 dataIndex;
@@ -319,7 +278,9 @@ out vec4 color;
 
 void main(){
 
-  color = abs(normalize(vec4(texture(velTex, dataIndex/numParticles).xyz,.75)));
+  color = vec4(abs(normalize(texture(velTex, dataIndex/numParticles).xyz)),1.0);
+
+  //color = abs(normalize(vec4(,.75)));
   gl_Position = mvp * texture(dataTex, dataIndex/numParticles);
   gl_PointSize = 2.0;
 }`;
@@ -335,8 +296,8 @@ in vec4 color;
 out vec4 fragColor;
 
 void main() {
-
     fragColor = color;
+    //fragColor = vec4(1.0,0.5,0.5,1.0);
 }
 `;
 
@@ -359,41 +320,6 @@ void main() {
 
 
 
-//Frag shader for updating position/velocity data textures
-
-var dataUpdateFrag = 
-`#version 300 es
-
-precision mediump float;
-precision mediump sampler3D;
-precision mediump sampler2D;
-
-uniform sampler2D dataTex;
-uniform sampler2D velTex;
-uniform sampler3D flowField;  
-
-uniform float timeDiff;
-
-in vec2 index;
-
-layout(location=0) out vec4 newPosition;
-layout(location=1) out vec4 newVelocity;
-
-
-void main(){
-    
-    vec3 pos = texture(dataTex, index).xyz; 
-    vec3 velocity = texture(velTex, index).xyz;
-
-    vec3 flow = .01 * texture(flowField, (pos.xyz/12.0)).xyz;
-    vec3 newPos = pos + .01 * timeDiff * velocity; 
-
-    newPosition = vec4(newPos.xyz, 1.0);
-    newVelocity = vec4((velocity + 1.0 * flow).xyz, 1.0);
-}`;
-
-
-
 //Gpass vertex shader
 
 var objGPassVert = 
@@ -403,8 +329,6 @@ precision mediump float;
 precision mediump sampler2D;
 
 uniform mat4 mvp;
-//uniform mat4 frust;
-
 
 layout(location=0) in vec3 normal;
 layout(location=1) in vec3 position;
@@ -485,30 +409,7 @@ void main(){
 
 
 
-//For debugging perlin noise 
 
-var noiseViz = 
-`#version 300 es
-
-precision mediump float;
-precision mediump sampler2D;
-
-uniform sampler2D noiseTex;
-
-in vec2 index;
-
-out vec4 fragColor;
-
-
-void main(){
-
-	vec4 noiseVal = texture(noiseTex, index);
-    fragColor = vec4(noiseVal.x, noiseVal.y, 0.0,1.0); 
-}`;
-
-
-
-//For debugging deferred pass...
 var depthVert = 
 `#version 300 es
 
@@ -528,18 +429,27 @@ void main(){
     fragColor = vec4(0.5, 0.5, 0.5, 1.0);
     gl_Position = mvp * vec4(position,1.0); 
 }`;
-//For debugging deferred pass...
+
+
 var depthFrag = 
 `#version 300 es
 
 precision mediump float;
 
+
 in vec4 fragColor;
+
 out vec4 color;
 
+
 void main(){
+
     color = fragColor; 
 }`;
+
+
+
+
 
 
 
@@ -560,7 +470,6 @@ if (!testExtension("EXT_color_buffer_float")) {
 
 let canvas = document.getElementById("view");
 
-//let gl = canvas.getContext("webgl2", );
 
 
 let app = PicoGL.createApp(canvas, {stencil:true})
@@ -595,89 +504,24 @@ for (let i = 0; i < NUM_PARTICLES * 4; i+=4){
     posIndicies[i/2] = (i/4) % dim;
     posIndicies[i/2 + 1] = Math.floor((i/4)/dim);
 
-    posData[i] = .5 * (Math.random() * 2 - 1);
-    posData[i + 1] = 2 * (Math.random() * 2 - 1);
-    posData[i + 2] = 1 * (Math.random() * 2 - 1);
+    posData[i] = (Math.random() * 2 - 1);
+    posData[i + 1] = (Math.random() * 2 - 1);
+    posData[i + 2] = (Math.random() * 2 - 1);
     posData[i + 3] = 1.0;
 
-    velData[i] = 0 * (Math.random() * 2 - 1);
-    velData[i + 1] = 0 * (Math.random() * 2 - 1);
+    velData[i] = 0.0
+    velData[i + 1] = 0.0
     velData[i + 2] = 0.0;
     velData[i + 3] = 0.0;
 }
 
 
 
-
 const noiseDim = 64;
 
-let textureData = new Float32Array(noiseDim * noiseDim * noiseDim * 4);
-let textureIndex = 0;
 
 
-function testBound(num,dimen){
-
-	let mid = dimen / 2;
-	let diff = num - mid;
-	let diffAbs = Math.abs(diff);
-
-	if (diffAbs > 10){
-		return (diffAbs/4 * (diff/diffAbs));
-	}
-	return 0;
-}
-
-
-
-for (let i = 0; i < noiseDim; ++i) {
-	let iBound = testBound(i, noiseDim);
-    for (let j = 0; j < noiseDim; ++j) {
-    	let jBound = testBound(j, noiseDim)
-        for (let k = 0; k < noiseDim; ++k) {
-        	let kBound = testBound(k, noiseDim)
-                
-            let iadj = .07*i;///noiseDim;
-            let jadj = .07*j;///noiseDim;
-            let kadj = .07*k;///noiseDim;
-
-            let x =noiseGen.noise(jadj,kadj) 
-            let y =noiseGen.noise(iadj,kadj)
-            let z =noiseGen.noise(iadj,jadj)
-
-            if (x < 0){
-              x *= .99;
-            } 
-
-            if (y > 0){
-              y *= .9
-            }
-            
-            textureData[textureIndex++] = x;// + iBound;
-            textureData[textureIndex++] = y;//+ jBound;
-            textureData[textureIndex++] = z;// + kBound;
-            textureData[textureIndex++] = 0;
-        }
-    }
-}
-
-
-let noiseTex = app.createTexture3D(textureData, noiseDim, noiseDim, noiseDim, { 
-    internalFormat: PicoGL.RGBA16F, 
-    minFilter: PicoGL.NEAREST,
-    magFilter: PicoGL.NEAREST,
-    wrapS: PicoGL.REPEAT,
-    wrapT: PicoGL.REPEAT
-});
-
-let noiseTexB = app.createTexture2D(textureData, noiseDim, noiseDim, { 
-    internalFormat: PicoGL.RGBA16F, 
-    minFilter: PicoGL.NEAREST,
-    magFilter: PicoGL.NEAREST,
-    wrapS: PicoGL.REPEAT,
-    wrapT: PicoGL.REPEAT
-});
-
-let dataTexA = app.createTexture2D(posData, dim, dim, {
+let startTex = app.createTexture2D(posData, dim, dim, {
     minFilter: PicoGL.NEAREST,
     magFilter: PicoGL.NEAREST,
     wrapS: PicoGL.REPEAT,
@@ -685,7 +529,7 @@ let dataTexA = app.createTexture2D(posData, dim, dim, {
     internalFormat: PicoGL.RGBA16F
 });
 
-let dataTexB = app.createTexture2D(posData, dim, dim, {
+let dataTex = app.createTexture2D(posData, dim, dim, {
     minFilter: PicoGL.NEAREST,
     magFilter: PicoGL.NEAREST,
     wrapS: PicoGL.REPEAT,
@@ -694,15 +538,7 @@ let dataTexB = app.createTexture2D(posData, dim, dim, {
 });
 
 
-let velTexA = app.createTexture2D(velData, dim, dim, { 
-    minFilter: PicoGL.NEAREST,
-    magFilter: PicoGL.NEAREST,
-    wrapS: PicoGL.REPEAT,
-    wrapT: PicoGL.REPEAT,
-    internalFormat: PicoGL.RGBA16F
-});
-
-let velTexB = app.createTexture2D(velData, dim, dim, { 
+let velTex = app.createTexture2D(velData, dim, dim, { 
     minFilter: PicoGL.NEAREST,
     magFilter: PicoGL.NEAREST,
     wrapS: PicoGL.REPEAT,
@@ -711,12 +547,6 @@ let velTexB = app.createTexture2D(velData, dim, dim, {
 });
 
 
-let threeTex = app.createTexture2D(app.width, app.height, {
-    internalFormat: PicoGL.RGBA16F
-});
-
-
-//let gNormalsArray = new Float32Array(canvas.width * canvas.height * 4);
 
 
 let gNormals = app.createTexture2D(app.width, app.height, {
@@ -741,14 +571,10 @@ let gBuffer = app.createFramebuffer(app.width, app.height)
 .colorTarget(2, gMaterial)
 .depthTarget(depthTarget);
 
-let threeTarget = app.createFramebuffer(app.width, app.height)
-.colorTarget(0, threeTex);
 
 
 
 console.assert(gBuffer.getStatus() === PicoGL.FRAMEBUFFER_COMPLETE, "G-buffer framebuffer is not complete!");
-
-
 
 
 let indices = app.createVertexBuffer(PicoGL.FLOAT, 2, posIndicies)
@@ -779,15 +605,10 @@ var catArray = app.createVertexArray()
 
 
 let updateFramebuffer = app.createFramebuffer(dim, dim)
-.colorTarget(0, dataTexB)
-.colorTarget(1, velTexA);
+.colorTarget(0, dataTex)
+.colorTarget(1, velTex);
 
-let noiseFramebuffer = app.createFramebuffer(noiseDim, noiseDim)
-.colorTarget(0, noiseTexB);
 
-/*let projMatrix = mat4.create();
-mat4.perspective(projMatrix, Math.PI / 2, canvas.width / canvas.height, 0.1, null);
-*/
 
 var frust = mat4.create();
 mat4.perspective(frust, Math.PI/2, 1000/700, .1, null);
@@ -802,8 +623,6 @@ var frameNum = 0;
 
 
 let quadShader = app.createShader(PicoGL.VERTEX_SHADER, quad_vs);
-
-let noiseVizShader = app.createShader(PicoGL.FRAGMENT_SHADER, noiseViz);
 let dataUpdateShader = app.createShader(PicoGL.FRAGMENT_SHADER, dataUpdateFrag);
 
 let pointLightVertShader = app.createShader(PicoGL.VERTEX_SHADER, pointLightVert);
@@ -831,19 +650,17 @@ var moved = false;
 app.createPrograms([pointLightVertShader, pointLightFragShader],
                    [pointVertShader, pointFragShader], 
                    [quadShader, dataUpdateShader], 
-                   [quadShader, noiseVizShader],
                    [objGPassVertShader, objGPassFragShader],
                    [quadShader, globalLightShader],
                    [depthVertShader, depthFragShader])
                   .then((
                     [pointLightProgram, pointProgram,
-                     updateProgram, noiseProgram, gPassProgram, 
+                     updateProgram, gPassProgram, 
                      globalLightProgram, depthProgram]
                    ) => {
     
     let updatePositionCall = app.createDrawCall(updateProgram, quadArray)
     .primitive(PicoGL.TRIANGLES)
-    .texture("flowField", noiseTex)
 
     let lightsPass = app.createDrawCall(pointLightProgram, points)
     .primitive(PicoGL.POINTS)
@@ -868,96 +685,7 @@ app.createPrograms([pointLightVertShader, pointLightFragShader],
     let depthPass = app.createDrawCall(depthProgram, catArray)
     .primitive(PicoGL.TRIANGLES)
 
-    /*
-    let noiseCall = app.createDrawCall(noiseProgram, quadArray)
-    .uniform("depth", 0.0)
-    .texture("noiseTex", noiseTex)
-    .primitive(PicoGL.TRIANGLES);*/
-
-    /*var renderer = new THREE.WebGLRenderer({ canvas: view, context: app.gl } );
-
-	  const WIDTH = app.width;
-    const HEIGHT = app.height;
-
-    // Set some camera attributes.
-    const VIEW_ANGLE = 45;
-    const ASPECT = WIDTH / HEIGHT;
-    const NEAR = 0.1;
-    const FAR = 10000;
-
-    // Get the DOM element to attach to
-    const container =
-        document.querySelector('#three');
-
-    // Create a WebGL renderer, camera
-    // and a scene
-    const renderer = new THREE.WebGLRenderer();
-    const camera =
-        new THREE.PerspectiveCamera(
-            VIEW_ANGLE,
-            ASPECT,
-            NEAR,
-            FAR
-        );
-
-    const scene = new THREE.Scene();
-
-    // Add the camera to the scene.
-    scene.add(camera);
-
-    // Start the renderer.
-    renderer.setSize(WIDTH, HEIGHT);
-
-    // Attach the renderer-supplied
-    // DOM element.
-    container.appendChild(renderer.domElement);
-
-    // create a point light
-    const pointLight =
-      new THREE.PointLight(0xFEFEFE);
-
-    // set its position
-    pointLight.position.x = 0;
-    pointLight.position.y = 50;
-    pointLight.position.z = 130;
-
-    // add to the scene
-    scene.add(pointLight);
-
-    // create the sphere's material
-    const sphereMaterial =
-      new THREE.MeshLambertMaterial(
-        {
-          color: 0xCC0000
-        });
-
-    // Set up the sphere vars
-    const RADIUS = 100;
-    const SEGMENTS = 16;
-    const RINGS = 16;
-
-
-    var canvasTexture = new THREE.CanvasTexture(canvas);
-    var canvasMaterial = new THREE.MeshBasicMaterial({ map: canvasTexture });
-
-
-    const sphere = new THREE.Mesh(
-
-      new THREE.SphereGeometry(
-        RADIUS,
-        SEGMENTS,
-        RINGS),
-
-      canvasMaterial);
-
-    // Move the Sphere back in Z so we
-    // can see it.
-    sphere.position.z = -300;
-
-    // Finally, add the sphere to the scene.
-    scene.add(sphere);
-    */
- 
+    
     
     function draw() {
 
@@ -1007,60 +735,27 @@ app.createPrograms([pointLightVertShader, pointLightFragShader],
 
         timeNow = performance.now()/1000;
 
-        updatePositionCall.uniform("timeDiff", timeNow - lastTime);
+        updatePositionCall.uniform("timer", timeNow*1000);
 
         lastTime = timeNow;
 
+        updatePositionCall.texture("startTex", startTex)
+        updateFramebuffer.colorTarget(0, dataTex)
+        updateFramebuffer.colorTarget(1, velTex)
+        
 
-        if (!flag){
-        	//updatePositionCall.texture("noiseTex", noiseTex)
-            updatePositionCall.texture("dataTex", dataTexA)
-            updatePositionCall.texture("velTex", velTexA)
-            updateFramebuffer.colorTarget(0, dataTexB)
-            updateFramebuffer.colorTarget(1, velTexB)
-
-            //noiseCall.texture("noiseTex", noiseTex);
-            //noiseFramebuffer.colorTarget(0, noiseTexB);
-        }
-        else {
-        	//updatePositionCall.texture("noiseTex", noiseTexB)
-            updatePositionCall.texture("dataTex", dataTexB)
-            updatePositionCall.texture("velTex", velTexB)
-            updateFramebuffer.colorTarget(0, dataTexA)
-            updateFramebuffer.colorTarget(1, velTexA)
-
-            //noiseCall.texture("noiseTex", noiseTexB);
-            //noiseFramebuffer.colorTarget(0, noiseTex)
-        }
         app.viewport(0, 0, dim, dim);
         app.drawFramebuffer(updateFramebuffer).noBlend().clear();
         updatePositionCall.draw();
 
 
-        if (!flag){
-            lightsPass.texture("dataTex", dataTexB);
-            lightsPass.texture("velTex", velTexB);
 
-            pointsPass.texture("dataTex", dataTexB);
-            pointsPass.texture("velTex", velTexB);
-        }
-        else {
-            lightsPass.texture("dataTex", dataTexA);
-            lightsPass.texture("velTex", velTexA);
+        lightsPass.texture("dataTex", dataTex);
+        lightsPass.texture("velTex", velTex);
+        pointsPass.texture("dataTex", dataTex);
+        pointsPass.texture("velTex", velTex);
+        
 
-            pointsPass.texture("dataTex", dataTexA);
-            pointsPass.texture("velTex", velTexA);
-
-        }
-        flag = !flag;
-
-        /*
-        app.viewport(0, 0, noiseDim, noiseDim);
-        app.drawFramebuffer(noiseFramebuffer)
-        .noBlend()
-        .depthMask(false);
-        noiseCall.draw();*/
-      
 
         app.defaultViewport();
         app.drawFramebuffer(gBuffer)
@@ -1079,7 +774,7 @@ app.createPrograms([pointLightVertShader, pointLightFragShader],
         .depthMask(false)
         .clear()
 
-        //globalLightPass.draw();
+        globalLightPass.draw();
 
 
 
@@ -1096,9 +791,9 @@ app.createPrograms([pointLightVertShader, pointLightFragShader],
 
 
         app.depthMask(false)
- 	    .blendFunc(PicoGL.SRC_COLOR, PicoGL.ONE)
- 		.stencilFunc(PicoGL.EQUAL, 1, 0xFF)
- 		.stencilMask(0);
+ 	      .blendFunc(PicoGL.SRC_COLOR, PicoGL.ONE)
+ 		    .stencilFunc(PicoGL.EQUAL, 1, 0xFF)
+ 		    .stencilMask(0);
 
 
         lightsPass.uniform("mvp", mvp);
@@ -1107,12 +802,6 @@ app.createPrograms([pointLightVertShader, pointLightFragShader],
         app.noStencilTest()
         pointsPass.uniform("mvp", mvp);
         pointsPass.draw();
-
-
-        //canvasTexture.needsUpdate = true;
-        //app.drawFramebuffer(threeTarget).blend().depthMask(false);
-        //renderer.render(scene, camera);
-
 
         timer.end();
 
